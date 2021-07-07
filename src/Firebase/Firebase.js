8dfa17434c7dad718f0edbc723ffc39c93f0ea5c
Ashar Ali <ashar.alia1999@gmail.com>
2021-07-08 00:47:47 +0500

    done with Firebase Config

---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBS8YJgeSA_UUhFR_bx-BQgyM9YCmX63BI",
+  authDomain: "linkedin-clone-2de1d.firebaseapp.com",
+  projectId: "linkedin-clone-2de1d",
+  storageBucket: "linkedin-clone-2de1d.appspot.com",
+  messagingSenderId: "554750931223",
+  appId: "1:554750931223:web:003775138d116362f24752",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firestore = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { firestore, auth };
